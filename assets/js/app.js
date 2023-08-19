cc76862aacb4e0ae3a57ece134b6d95da9172882
subham8723 <120543389+subham8723@users.noreply.github.com>
2023-08-19 22:47:02 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -100,7 +100,7 @@ addEventOnElements($tabBtns, "keydown", function (e) {
 
 const /** {NodeElement} */ $searchSubmit = document.querySelector("[data-search-submit]");
 
-let /** {String} */ apiUrl = "https://api.github.com/users/mritunjay203";
+let /** {String} */ apiUrl = "https://api.github.com/users/subham8723";
 let /** {String} */ repoUrl, followerUrl, followingUrl = "";
 
 const searchUser = function () {
